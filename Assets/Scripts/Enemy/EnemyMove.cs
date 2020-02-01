d11d63312d37b1b48b14141194aee200e0a3a99f
rodriarce <rodrigoarce91@gmail.com>
2020-02-01 19:18:58 -0300

    Move

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class EnemyMove : MonoBehaviour {
 
     [Header("Settings")]
     public int bulletsPerSecond = default;
-    public float speed = 3f;
+    public float speed = 100f;
     public bool moveUp = true;
     public bool moveDown = true;
 
@@ -15,18 +16,33 @@ public class EnemyMove : MonoBehaviour {
     public Bullet prefabBullet = default;
 
     private float delay;
+    private float startTime;
     private float nextShoot;
-
+    public GameObject[] shootPoints;
     public static Pooling<Bullet> pooling;
+    private IEnumerator coroutine;
 
     void Start() {
         pooling = new Pooling<Bullet>(20, prefabBullet, new GameObject("PoolObjects Bullet").transform, false);
         delay = 1 / bulletsPerSecond;
+        startTime=Time.time;
+        
     }
 
     void Update() {
-        MoveUpDown();
+        //MoveUpDown();
+        float timeElapse=3f;
+
+
+        if(Time.time>startTime+timeElapse)
+        {
+             MoveRandom();
+        
+        startTime=Time.time;
+        }
         ShootBullet();
+        
+       
     }
 
     private void ShootBullet() {
@@ -38,26 +54,29 @@ public class EnemyMove : MonoBehaviour {
         }
     }
 
-    private void MoveUpDown() {
-        if (moveUp) {
-            transform.Translate(transform.up * Time.deltaTime * speed);
-            transform.position = new Vector2(transform.position.x, transform.position.y);
-        };
-        if (transform.position.y >= 4) {
-            transform.position = new Vector2(transform.position.x, transform.position.y);
-            moveUp = false;
-            moveDown = true;
-        }
-        if (!moveUp) {
-            transform.Translate(-transform.up * Time.deltaTime * speed);
-        }
-        if (transform.position.y <= -4) {
-            transform.position = new Vector2(transform.position.x, transform.position.y);
-            moveDown = false;
-            moveUp = true;
-        }
-        if (!moveDown) {
-            transform.Translate(transform.up * Time.deltaTime * speed);
-        }
-    }
+
+            private void MoveRandom()
+            {
+                   int pointGame=Random.Range(0,shootPoints.Length);
+
+            
+                            
+                              
+                        
+                                                 
+                            transform.position=Vector2.MoveTowards(transform.position,shootPoints[pointGame].transform.position,Time.deltaTime * speed);
+                            
+                    
+                    
+                    //transform.Translate(new Vector2(0,shootPoints[pointGame].transform.position.y)* Time.deltaTime *speed, Space.Self);
+
+            }
+
+           
+
+
+
+
+
+
 }

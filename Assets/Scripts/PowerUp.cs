8ac65df3759ee90106bbd71f2b076184f94c7027
Daniel-ECB <danielcorzo@outlook.com>
2020-02-01 16:19:53 -0300

    Created first PowerUP.

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        //Debug.Log("The player has pressed the PowerUp");
+
+    }
+}

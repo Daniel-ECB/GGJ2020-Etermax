f4d53ced53dbc0fb6a945ab86f5eb682d88ff1de
UtopicoStudio <utopicostudio.es@gmail.com>
2020-02-01 16:49:17 -0300

    Todo un poquito mas prolijo

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class Bullet : PoolObject {
 
-  [Header("Settings")]
-  public float bulletSpeed=3f;
-  public float rateBullet=5f;
+    [Header("Settings")]
+    public float bulletSpeed = 3f;
+    public float rateBullet = 5f;
 
-  private void Update(){
-    transform.Translate(transform.right * bulletSpeed * Time.deltaTime*rateBullet);
-  }
-
-  private void OnTriggerEnter2D(Collider2D other){
-    if (other)
-    {
-      EnemyMove.pooling.Release(this);
-      gameObject.SetActive(false);
+    private void Update() {
+        transform.Translate(transform.right * bulletSpeed * Time.deltaTime * rateBullet);
     }
-  }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other) {
+            EnemyMove.pooling.Release(this);
+            gameObject.SetActive(false);
+        }
+    }
 }
--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -1,14 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyMove : MonoBehaviour {
 
     [Header("Settings")]
+    public int bulletsPerSecond = default;
     public float speed = 3f;
     public bool moveUp = true;
     public bool moveDown = true;
-
 
     [Header("References")]
     public Transform spawnBullet = default;
@@ -16,23 +14,28 @@ public class EnemyMove : MonoBehaviour {
     [Header("Assets")]
     public Bullet prefabBullet = default;
 
+    private float delay;
+    private float nextShoot;
+
     public static Pooling<Bullet> pooling;
 
     void Start() {
         pooling = new Pooling<Bullet>(20, prefabBullet, new GameObject("PoolObjects Bullet").transform, false);
-
+        delay = 1 / bulletsPerSecond;
     }
 
     void Update() {
         MoveUpDown();
         ShootBullet();
-
     }
 
     private void ShootBullet() {
-        Bullet bullet = pooling.Get();
-        bullet.transform.position = spawnBullet.position;
-        bullet.transform.rotation=spawnBullet.rotation;
+        if (nextShoot <= Time.time) {
+            nextShoot = Time.time + delay;
+            Bullet bullet = pooling.Get();
+            bullet.transform.position = spawnBullet.position;
+            bullet.transform.rotation = spawnBullet.rotation;
+        }
     }
 
     private void MoveUpDown() {

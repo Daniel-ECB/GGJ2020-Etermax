e485d252247f2143896540f7b1a8370b42100e00
bkavinoff <bkavinoff@outlook.com>
2020-02-01 14:21:05 -0300

    Adding scripts

---
+++ Assets/Scripts/Player/MotherShip.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherShip : MonoBehaviour, IAttackable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void Kill()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+}
---
+++ Assets/Scripts/Player/Ship.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour, IAttackable
+{
+    public int health;
+    public int damage;
+    public GameObject target;
+
+
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        this.health -= damage;
+        if (this.health <= 0)
+            Kill();
+
+        //End of game
+    }
+}

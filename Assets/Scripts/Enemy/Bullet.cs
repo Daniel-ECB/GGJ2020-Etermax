8cf81e3aa85b356a15e93d3fb31dacbf2234b335
rodriarce <rodrigoarce91@gmail.com>
2020-02-01 16:14:32 -0300

    Update Bullet Script

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -2,6 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : PoolObject, IPooling {
+public class Bullet : PoolObject {
 
+  [Header("Settings")]
+  public float bulletSpeed=3f;
+  public float rateBullet=5f;
+
+  private void Update(){
+    transform.Translate(transform.right * bulletSpeed * Time.deltaTime*rateBullet);
+  }
+
+  private void OnTriggerEnter2D(Collider2D other){
+    if (other)
+    {
+      EnemyMove.pooling.Release(this);
+      gameObject.SetActive(false);
+    }
+  }
 }

fa576f845f74d6c5ff6cad36ef675d452e858ea3
rodriarce <rodrigoarce91@gmail.com>
2020-02-01 12:40:53 -0300

    HOla subo archivo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    public GameObject gameMenu; 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(gameManager==null)
+        {
+
+            gameManager=this;
+
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

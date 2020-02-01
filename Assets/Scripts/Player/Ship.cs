423a251cbed636df8aaf25d735b205ed7fd8915a
bkavinoff <bkavinoff@outlook.com>
2020-02-01 14:27:35 -0300

    scripts

--- Assets/Scripts/Player/Ship.cs
+++ Assets/Scripts/Player/Ship.cs
@@ -8,6 +8,17 @@ public class Ship : MonoBehaviour, IAttackable
     public int damage;
     public GameObject target;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void Kill()
     {

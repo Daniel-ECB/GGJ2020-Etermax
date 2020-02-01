c5b7a7bf47c106dc4da21018d3eee669caefb7b5
UtopicoStudio <utopicostudio.es@gmail.com>
2020-02-01 17:42:09 -0300

    Nuevo Background y TextMeshPro

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour {
+
+    [Header("Settings")]
+    [SerializeField] private float boundaryLeft = default;
+    [SerializeField] private float boundaryRight = default;
+    [SerializeField] private float speed = default;
+
+    [Header("References")]
+    [SerializeField] private Transform nebule = default;
+
+    private void Update() {
+        nebule.Translate(new Vector3(Time.deltaTime * speed, 0));
+
+        if (nebule.position.x >= boundaryRight) {
+            nebule.position = new Vector3(boundaryLeft, 0);
+        }
+    }
+}

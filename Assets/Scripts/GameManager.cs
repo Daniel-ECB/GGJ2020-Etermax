ea0be53a5816cb7a12846f2c780fedf614cdbb8c
UtopicoStudio <utopicostudio.es@gmail.com>
2020-02-01 19:35:34 -0300

    Avances con la Interfaz

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
-    public static GameManager gameManager;
-    public GameObject gameMenu; 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        if(gameManager==null)
-        {
+public class GameManager : MonoBehaviour {
 
-            gameManager=this;
+    [Header("Settings")]
+    [SerializeField] private string titleWin = default;
+    [SerializeField] private string commentWin = default;
+    [Space]
+    [SerializeField] private string titleLose = default;
+    [SerializeField] private string commentLose = default;
 
+    [Header("References")]
+    [SerializeField] private GameObject mainMenu = default;
+    [SerializeField] private Button buttonPlay = default;
+    [SerializeField] private Button buttonCredits = default;
+    [SerializeField] private Button buttonBack = default;
+    [Space]
+    [SerializeField] private GameObject endMenu = default;
+    [SerializeField] private TextMeshProUGUI textResult = default;
+    [SerializeField] private TextMeshProUGUI textComment = default;
+    [SerializeField] private Button buttonRestart = default;
+    [Space]
+    [SerializeField] private Animator animator = default;
 
-        }
+    public Action onStartGame;
+    public Action onGameOver;
 
+    private readonly int CONST_PLAY = Animator.StringToHash("Play");
+    private readonly int CONST_CREDITS = Animator.StringToHash("Credits");
+    private readonly int CONST_BACK = Animator.StringToHash("Back");
+
+    public static GameManager instance;
+
+    private void Awake() {
+        instance = this;
+
+        buttonPlay.onClick.AddListener(OnClickPlay);
+        buttonCredits.onClick.AddListener(OnClickCredits);
+        buttonBack.onClick.AddListener(OnClickBack);
+
+        buttonRestart.onClick.AddListener(OnClickRestart);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void OnClickPlay() {
+        onStartGame?.Invoke();
+        animator.SetTrigger(CONST_PLAY);
+    }
 
+    private void OnClickCredits() {
+        animator.SetTrigger(CONST_CREDITS);
+    }
+
+    private void OnClickBack() {
+        animator.SetTrigger(CONST_BACK);
+    }
+
+    private void OnClickRestart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GameOver(bool win) {
+        textResult.text = win ? titleWin : titleLose;
+        textComment.text = win ? commentWin : commentLose;
+        endMenu.SetActive(true);
     }
 }

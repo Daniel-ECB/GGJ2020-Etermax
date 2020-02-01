63890c31014e215faae6ff726a88c1958c3c1b2c
rodriarce <rodrigoarce91@gmail.com>
2020-02-01 14:09:56 -0300

    Moviento de la nave.

---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float speed=3f;
+    public bool moveUp=true;
+    public bool moveDown=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          MoveUpDown();
+
+
+    }
+
+
+    public void MoveUpDown()
+    {
+
+
+      if (moveUp)
+      {
+      transform.Translate(transform.up * Time.deltaTime * speed);
+
+      transform.position= new Vector2(transform.position.x,transform.position.y);
+
+
+    };
+
+
+
+    if(transform.position.y>=4)
+    {
+      transform.position= new Vector2(transform.position.x,transform.position.y);
+
+
+      moveUp=false;
+      moveDown=true;
+
+
+
+    }
+    if(!moveUp)
+    {
+
+      transform.Translate(-transform.up * Time.deltaTime * speed);
+
+    }
+
+    if(transform.position.y<=-4)
+    {
+      transform.position= new Vector2(transform.position.x,transform.position.y);
+
+      moveDown=false;
+      moveUp=true;
+
+
+    }
+    if(!moveDown)
+    {
+
+      transform.Translate(transform.up * Time.deltaTime * speed);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
